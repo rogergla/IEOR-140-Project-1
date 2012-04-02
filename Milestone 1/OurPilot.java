e174ceb3924a3170d295aa17ceaa2a6ff893a7f5
Shouvik Dutta <shodutta92@gmail.com>
2012-04-02 14:09:50 -0700

    adding Milestone 1

---
+++ Milestone 1/OurPilot.java
@@ -0,0 +1,51 @@
+
+import lejos.nxt.*;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Team 3
+ */
+public class OurPilot {
+    
+    boolean clockwise;
+    float track_width;
+    float diameter;
+    
+    
+    public OurPilot(float diameter, float track_width) {
+        this.track_width = track_width;
+        this.diameter = diameter;
+
+    }
+
+
+    void rotate(int angle, boolean clockwise) {
+        float amount_rotate = angle * track_width / diameter;
+
+        if (clockwise) {
+            Motor.B.rotate((int) amount_rotate, true);
+            Motor.A.rotate((int) (-1 * amount_rotate));
+        } else {
+            Motor.B.rotate((int) (-1 * amount_rotate), true);
+            Motor.A.rotate((int) amount_rotate);
+        }
+    }
+    
+    void stop() {
+        Motor.A.stop(true);
+        Motor.B.stop();
+    }
+    
+    void travel(double distance) {
+        double rotate_angle = 360* distance/(Math.PI * diameter);
+        
+        
+        
+        Motor.A.rotate((int) rotate_angle, true);
+        Motor.B.rotate((int) rotate_angle);
+    }
+}
