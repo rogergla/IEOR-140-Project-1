ea067e75e599cef24cc2c0ae761fe2049a7397e7
Shouvik Dutta <shodutta92@gmail.com>
2012-04-02 14:00:34 -0700

    test branch

--- Milestone 2/Milestone2.java
+++ Milestone 2/Milestone2.java
@@ -3,33 +3,33 @@ import lejos.nxt.*;
 import lejos.robotics.navigation.DifferentialPilot;
 import lejos.util.Delay;
 
-
 /**
  *
  * @author glassey
  */
-public class Milestone1 {
+public class Milestone2 {
 
     public static void main(String[] args) {
         System.out.println(" Hello World");
         Button.waitForAnyPress();
         Delay.msDelay(2000);
-
-        OurPilot pilot = new OurPilot(5.6f,12.5f);
-
+        DifferentialPilot pilot = new DifferentialPilot(5.6f, 11.75f, Motor.A, Motor.B, false);
+        pilot.setRotateSpeed(90);
+//      pilot.setTravelSpeed(400);
         for (int i = 0; i < 8; i++) {
             pilot.travel(90);
             pilot.stop();
-            pilot.rotate(90,true);
+            pilot.rotate(90);
             pilot.stop();
-
+//        System.out.println("tachoA" + Motor.A.getTachoCount());
+//        System.out.println("tachoB" + Motor.B.getTachoCount());
         }
 
-        pilot.rotate(90, true);
+        pilot.rotate(90);
         for (int i = 0; i < 8; i++) {
             pilot.travel(90);
             pilot.stop();
-            pilot.rotate(90,false);
+            pilot.rotate(-90);
             pilot.stop();
         }
 //      Button.waitForAnyPress();
